0fa2ae613162175af6595cd848355d05dee023c4
Michal Borek <michal@greenpath.pl>
2015-10-25 23:22:00 +0100

    Add zuul proxy

--- app/web/src/main/java/com/nokia/knowcamp/web/WebApplication.java
+++ app/web/src/main/java/com/nokia/knowcamp/web/WebApplication.java
@@ -3,9 +3,10 @@ package com.nokia.knowcamp.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-
+@EnableZuulProxy
 @EnableDiscoveryClient
 public class WebApplication {
 
